ce3169cb6773a9667dcc27167c49431e9e0f3523
WengerM996 <maks.wenger@yandex.ru>
2021-01-30 11:17:15 +0300

    init

---
+++ Assets/Scripst/BallLauncher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallLauncher : MonoBehaviour
+{
+    [SerializeField] private float _force;
+    [SerializeField] private Vector3 _direction;
+    private Rigidbody _rigidBody;
+    private bool _isBallLaunched;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        Debug.LogWarning("Click to launch ball");
+    }
+
+    private void Update()
+    {
+        if (_isBallLaunched) return;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isBallLaunched = true;
+            Launch();
+        }
+    }
+
+    private void Launch()
+    {
+        _rigidBody.AddForce(_direction * _force, ForceMode.Impulse);
+    }
+}
